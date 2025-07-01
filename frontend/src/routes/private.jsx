2689fb5c1ab58ad807ecf0b60e959e3a4091133a
liljoker06 <lahrimialadin@gmail.com>
2025-07-01 15:24:59 +0200

    Ajout de la page de profil avec gestion des informations utilisateur et option de suppression de compte

--- frontend/src/routes/private.jsx
+++ frontend/src/routes/private.jsx
@@ -1,11 +1,13 @@
 import { Route } from 'react-router-dom';
 import Conversation from '../pages/Conversation';
 import Settings from '../pages/Settings';
+import Profile from '../pages/Profile';
 
 const PrivateRoutes = (
   <>
     <Route path="/conversation" element={<Conversation />} />
     <Route path="/settings" element={<Settings />} />
+    <Route path="/profile" element={<Profile />} />
   </>
 );
 
