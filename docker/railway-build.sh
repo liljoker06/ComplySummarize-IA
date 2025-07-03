197c04e65843eb23f74492e973f0a5530fe585a0
liljoker06 <lahrimialadin@gmail.com>
2025-07-03 16:19:40 +0200

    Suppression des scripts railway-build.sh et railway-start.sh ainsi que du fichier de configuration .railpack.toml

--- docker/railway-build.sh
+++
@@ -1,2 +0,0 @@
-echo "▶ Building all services..."
-docker compose --env-file docker.env -f docker-compose.yaml build
--- docker/railway-start.sh
+++
@@ -1,2 +0,0 @@
-echo "🚀 Starting all services..."
-docker compose --env-file docker.env -f docker-compose.yaml up
